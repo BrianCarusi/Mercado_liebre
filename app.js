538d44ed69de1afc1a20c09e38ed422bdc2fae88
brayan <carusibrian@gmail.com>
2023-06-26 15:05:43 -0300

    tp hecho

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = 3030;
+
+//configuraciones
+app.use(express.static('public'));
+
+// Ruta
+app.get('/',(req,res) => res.sendFile(path.join(__dirname,'views','home.html')));
+
+app.listen(PORT, () => console.log(`Servidor corriendo en http://localhost:${PORT}`));
